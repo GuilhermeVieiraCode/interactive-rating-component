982b348fc01edbf2595329d77425fac1b6dee195
Guilherme Vieira <gui.vieira.code@gmail.com>
2022-04-09 13:37:16 -0300

    Add event listeners on script file

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const firstSection = document.querySelector("section:first-child");
-const secSection = document.querySelector("#sec-section");
-const p = document.querySelector("#sec-section div:nth-child(2) p");
+const firstContainer = document.querySelector(".container:first-child");
+const secondContainer = document.getElementById("sec-section");
+const ratingResult = document.querySelector("#sec-section .card div:first-child");
+const ratingBtns = document.getElementsByClassName("rating-btn");
+const submitBtn = document.getElementById('submit')
+secondContainer.style = "display: none";
 let value;
-secSection.style = "display: none";
 
-function option(button){
-    value = button.value;
+function handler(e){
+    value = e.currentTarget.value;
 }
 
 function submit(){
     if(value){
-        firstSection.style = "display: none";
-        p.innerHTML = `You selected ${value} out of 5`;
-        secSection.style = "display: true";
+        firstContainer.style = "display: none";
+        const content = `<div><p id="rating-result-content">You selected ${value} out of 5</p></div>`
+        ratingResult.insertAdjacentHTML('beforeend', content);
+        secondContainer.style = "display: true";
     }else{
-        alert("Select a option before submit")
+        alert("Please select a option before click submit button")
     }
 }
+
+
+for(i of ratingBtns){
+    i.addEventListener('click', handler);
+}
+
+submitBtn.addEventListener('click', submit);
